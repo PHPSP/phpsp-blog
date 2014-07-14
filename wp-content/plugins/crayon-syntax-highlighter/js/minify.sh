53965ff6deb5c7bb5609d33b3c0ec268d050040e
William Espindola <williamespindola@instinfo.com.br>
2014-07-14 10:23:25 -0300

    remove wp files

--- wp-content/plugins/crayon-syntax-highlighter/js/minify.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-BASEDIR=$(dirname $0)
-cd $BASEDIR
-
-source ../util/minify.sh
-
-common=$"$INPUT_PATH/util.js $INPUT_PATH/jquery.popup.js $INPUT_PATH/crayon.js $INPUT_PATH/cssjson.js"
-minify $common $OUTPUT_PATH/crayon.min.js
-minify $common $TE_PATH/crayon_qt.js $COLORBOX_PATH/jquery.colorbox-min.js $TE_PATH/crayon_tag_editor.js $OUTPUT_PATH/crayon.te.min.js
